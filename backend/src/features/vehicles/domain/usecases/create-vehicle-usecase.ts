44a3a3eec43ed369ea8df59ca5d62e3230c482ed
Ortiz Pergher <ortizpergher@gmail.com>
2022-07-07 19:14:54 -0300

    feat: create vehicle usecase

---
+++ backend/src/features/vehicles/domain/usecases/create-vehicle-usecase.ts
@@ -0,0 +1,27 @@
+import { UseCase } from '@core/domain/contracts/usecase';
+import { VehicleRepository } from '@features/vehicles/repositories/vehicle-repository';
+import { CreateVehicleDTO } from '../dtos';
+import { AppError } from '@core/domain/errors/app-error';
+
+export class CreateVehicleUseCase implements UseCase {
+  constructor(private vehicleRepository: VehicleRepository) {}
+
+  async run({ licensePlate, brand, model, version, year, enable }: CreateVehicleDTO) {
+    const findVehicle = await this.vehicleRepository.findByLicensePlate(licensePlate);
+
+    if (findVehicle) {
+      throw new AppError('Vehicle already exists');
+    }
+
+    const created = await this.vehicleRepository.create({
+      licensePlate,
+      brand,
+      model,
+      version,
+      year,
+      enable,
+    });
+
+    return created;
+  }
+}
