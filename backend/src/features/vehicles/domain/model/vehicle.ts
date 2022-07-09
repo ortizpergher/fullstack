ea66da4dfaeba579f3682c5f02579a23b34ef0ad
Ortiz Pergher <ortizpergher@gmail.com>
2022-07-08 22:50:04 -0300

    feat: add get all vehicle repository

--- backend/src/features/vehicles/domain/model/vehicle.ts
+++ backend/src/features/vehicles/domain/model/vehicle.ts
@@ -7,3 +7,13 @@ export interface Vehicle {
   year: number;
   enable: boolean;
 }
+
+export interface VehicleList {
+  id: string;
+  licensePlate: string;
+  brand: string;
+  model: string;
+  version: string;
+  year: number;
+  enable: boolean;
+}
--- backend/src/features/vehicles/infra/repositories/vehicle.repository.ts
+++ backend/src/features/vehicles/infra/repositories/vehicle.repository.ts
@@ -1,9 +1,27 @@
 import { pgHelper } from '@core/infra/connections/pg-helper';
 import { VehicleEntity } from '@core/infra/database/entities';
 import { CreateVehicleDTO } from '../../domain/dtos';
-import { Vehicle } from '../../domain/model';
+import { Vehicle, VehicleList } from '../../domain/model';
 
 export class VehicleRepository {
+  async getAllVehicle(): Promise<VehicleList[]> {
+    const repository = await pgHelper.getRepository(VehicleEntity);
+
+    const findVehicles = await repository.find();
+
+    const vehicles: VehicleList[] = findVehicles.map((v) => ({
+      id: v.id as string,
+      licensePlate: v.licensePlate,
+      brand: v.brand,
+      model: v.model,
+      version: v.version as string,
+      year: v.year,
+      enable: v.enable as boolean,
+    }));
+
+    return vehicles;
+  }
+
   async findByLicensePlate(licensePlate: string): Promise<VehicleEntity | null> {
     const repository = await pgHelper.getRepository(VehicleEntity);
 
